28c7d3107618578f33036a8afd3e528cf16fde2d
markb <markbarton@gmail.com>
2018-02-27 12:01:15 +0000

    Latest Changes for Video

--- server.js
+++ server.js
@@ -1,63 +1,38 @@
-const express = require('express');
-const path = require('path');
-const pjson = require('./package.json');
-const port = ('port', process.env.PORT || 8088);
+const express = require("express");
+const path = require("path");
+const pjson = require("./package.json");
+const port = ("port", process.env.PORT || 8088);
 const app = express();
 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+// Serve static files
+const serveStatic = require("serve-static");
 
-const serveStatic = require('serve-static');
+// Extract files from form data
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-const vision = require('@google-cloud/vision');
+// Google cloud library for Vision API
+const vision = require("@google-cloud/vision");
 
-
+// Google cloud client
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: 'keyfile.json'
+  keyFilename: "keyfile.json"
 });
 
 // Image Properties
-app.post('/color', upload.single('file'), function (req, res, next) {
-    console.log(req.file)
-    client.imageProperties(req.file.path)
-        .then((results) => {
-            // Extract Dominant Colors
-            const properties = results[0].imagePropertiesAnnotation;
-            const colors = properties.dominantColors.colors;
-            res.send(colors);
-        }).catch(err => {
-            console.error('ERROR:', err);
-            res.status(400).send('ERROR:', err);
-        });
-})
+app.post("/color", upload.single("file"), function(req, res, next) {
+  
+    client
+    .imageProperties(req.file.path)
+    .then(results => {
+      // send result data
+      res.send(results);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
 
-// Text Extraction
-app.post('/text', upload.single('file'), function (req, res, next) {
-    client.textDetection(req.file.path)
-        .then((results) => {
-            // Extract Dominant Colors
-            const detections = results[0].textAnnotations;
-            res.send(detections);
-        }).catch(err => {
-            console.error('ERROR:', err);
-            res.status(400).send('ERROR:', err);
-        });
-})
-
-// Label Extraction
-app.post('/label', upload.single('file'), function (req, res, next) {
-    client.labelDetection(req.file.path)
-        .then((results) => {
-            // Extract Dominant Colors
-            const labels = results[0].labelAnnotations;
-            res.send(labels);
-        }).catch(err => {
-            console.error('ERROR:', err);
-            res.status(400).send('ERROR:', err);
-        });
-})
-
-
-app.use(serveStatic(path.join(__dirname, 'public')))
+app.use(serveStatic(path.join(__dirname, "public")));
 const server = app.listen(port);
 console.log(`😁  ${pjson.name} running → PORT ${server.address().port}`);
